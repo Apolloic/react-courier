ec6632a03e83cbd89576873de5364381d1175770
ierfan <ierfan@ierfans-Laptop.local>
2023-04-02 22:51:42 +0330

    fix: fix export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-export { CreateRestHook } from './core/CreateApi'
 import { RestHookProvider } from './providers/RestHookProvider'
-export type { RHookTypeHelper, RHookMethodTypeHelper } from './types/types'
+
+export { CreateRestHook } from './core/CreateApi'
 export { ReactQueryDevtools as RestHookDevtools } from '@tanstack/react-query-devtools'
 export { useQueryClient as useRestHook } from '@tanstack/react-query'
 export interface RegisterErrorDto {}
 export interface RegisterOtherBaseUrls {}
 export default RestHookProvider
+
+export type { RHookTypeHelper, RHookMethodTypeHelper } from './types/types'
