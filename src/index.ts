9b7b23118f37965842e0698952b71adcaf2e368b
ierfan <ierfan@ierfans-Laptop.local>
2023-04-03 01:13:42 +0330

    refactor: change default export to name export

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import { RestHookProvider } from './providers/RestHookProvider'
-
+export { RestHookProvider } from './providers/RestHookProvider'
 export { CreateRestHook } from './core/CreateApi'
 export { ReactQueryDevtools as RestHookDevtools } from '@tanstack/react-query-devtools'
 export { useQueryClient as useRestHook } from '@tanstack/react-query'
+
 export interface RegisterErrorDto {}
 export interface RegisterOtherBaseUrls {}
-export default RestHookProvider
-
 export type { RHookTypeHelper, RHookMethodTypeHelper } from './types/types'
