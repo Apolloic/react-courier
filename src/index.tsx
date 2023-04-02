62faad0642147f942b34744c5e76346da2cf5321
ierfaaan <erfan.ab9898@gmail.com>
2023-04-02 16:54:21 +0430

    fix:export default CreateRestHook function

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
-export { CreateRestHook } from './core'
+import { CreateRestHook } from './core'
 export { RestHookProvider } from './providers'
 export type { RHookTypeHelper, RHookMethodTypeHelper } from './types'
 export { ReactQueryDevtools as RestHookDevtools } from '@tanstack/react-query-devtools'
 export { useQueryClient as useRestHook } from '@tanstack/react-query'
 export interface RegisterErrorDto {}
 export interface RegisterOtherBaseUrls {}
+export default CreateRestHook
