8866ac5002d5af19ff37f5d7cde513732c9ea868
ierfan <ierfan@ierfans-Laptop.local>
2023-04-03 16:09:14 +0330

    chore: add lerna

--- example/src/api/post.ts
+++
@@ -1,7 +0,0 @@
-import { CreateRestHook } from 'rest-hook'
-
-export const usePost = CreateRestHook({
-  method: 'GET',
-  name: ['post'],
-  endPoint: '/posts',
-})
--- example/src/main.tsx
+++ example/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { RestHookProvider } from "rest-hook";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <RestHookProvider defaultBaseUrl="">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </RestHookProvider>
+);
