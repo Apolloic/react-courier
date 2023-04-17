3da865fb4527911601c68873b0f13710cf93627c
er.abbasi <er.abbasi@emofid.com>
2023-04-17 16:55:42 +0330

    chore: add external to tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,7 @@ import type { Options } from 'tsup'
 
 export const tsup: Options = {
   splitting: true,
+  external: ['react', 'react-dom', 'lodash', 'axios', '@tanstack/react-query', '@tanstack/react-query-devtools'],
   clean: true, // clean up the dist folder
   dts: true, // generate dts files
   treeshake: true,
