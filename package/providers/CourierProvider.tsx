c21f88a5ebcc01a7f05ab494ac604b01a37773ad
parsakhosravani <parsakhosravani@gmail.com>
2024-10-30 20:15:23 +0330

    🚧 add hydation

--- package/providers/CourierProvider.tsx
+++ package/providers/CourierProvider.tsx
@@ -1,20 +1,22 @@
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { QueryClientProvider, QueryClient, Hydrate } from '@tanstack/react-query'
 import CourierContextProvider from './CourierContextProvider'
 import { CourierProviderPropsType } from '../types'
 import React from 'react'
-const queryClient = new QueryClient()
 
 export function CourierProvider(props: CourierProviderPropsType) {
+  const [queryClient] = React.useState(() => new QueryClient())
   return (
     <QueryClientProvider client={queryClient}>
-      <CourierContextProvider
-        otherBaseUrl={props.otherBaseUrl}
-        defaultOptions={props.defaultOptions}
-        defaultBaseUrl={props.defaultBaseUrl}
-        middleware={props.middleware}
-      >
-        {props.children}
-      </CourierContextProvider>
+      <Hydrate state={props.dehydratedState}>
+        <CourierContextProvider
+          otherBaseUrl={props.otherBaseUrl}
+          defaultOptions={props.defaultOptions}
+          defaultBaseUrl={props.defaultBaseUrl}
+          middleware={props.middleware}
+        >
+          {props.children}
+        </CourierContextProvider>
+      </Hydrate>
     </QueryClientProvider>
   )
 }
